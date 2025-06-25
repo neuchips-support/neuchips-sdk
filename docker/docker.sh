32a38c188577da666b4c067c43084671ee3791bb
Rock Chen <rock.chen@neuchips.ai>
2025-06-25 17:04:24 +0800

    Add docker files and start-neuchips-service.sh
    The files are cloned from
    https://github.com/neuchips-support/neuchips-sdk/releases/tag/LLM_SDK_1.4.1
    
    Signed-off-by: Rock Chen <rock.chen@neuchips.ai>

---
+++ docker/docker.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+DOCKER_IMAGE="neuchips/viper_prod:v1.4.1"
+
+CMD=$1
+
+if [ -z $CMD ]; then
+    echo
+    echo "Usage:"
+    echo " $0 build"
+    echo " $0 stop <env_file>"
+    echo " $0 run <env_file>"
+    echo
+    exit 0
+fi
+
+shift
+
+case $CMD in
+    build)
+        docker build -f ./Dockerfile -t ${DOCKER_IMAGE} . --no-cache --rm
+        ;;
+    stop)
+        if [ -z $1 ]; then
+            ENV="env_llama3"
+        else
+            ENV="$1"
+        fi
+
+        source $ENV
+
+        docker stop ${CONTAINER_NAME}
+        ;;
+    run)
+        if [ -z $1 ]; then
+            ENV="env_llama3"
+        else
+            ENV="$1"
+        fi
+
+        source $ENV
+
+        docker run -d -it --privileged --rm -w=/neuchips/ \
+            --name ${CONTAINER_NAME} \
+            -v /dev:/dev \
+            -p $PORT:$PORT \
+            --env-file $ENV \
+            -v ${HOST_MODEL_FOLDER}:${DOCKER_MODEL_FOLDER} \
+            -v ${HOST_CACHE_FOLDER}:${DOCKER_CACHE_FOLDER} \
+            ${DOCKER_IMAGE}
+        ;;
+    *)
+esac
---
+++ docker/start-neuchips-service.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SDK_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+LLAMA_ROOT=${SDK_ROOT}/llama.cpp
+MODEL_PATH="${DOCKER_MODEL_FOLDER}/${MODEL}"
+CACHE_PATH="${DOCKER_CACHE_FOLDER}/data.${MODEL}.${PORT}"
+[ -z ${CHAT_TEMPLATE} ] && CHAT_TEMPLATE=llama2
+
+export LD_LIBRARY_PATH=/neuchips/conda/envs/neutorch/lib:/neuchips/conda/envs/neutorch/lib/python3.10/site-packages/torch/lib
+
+mkdir -p ${CACHE_PATH}
+
+${LLAMA_ROOT}/llama-server ${DEBUG} -m ${MODEL_PATH} \
+    -np 1 \
+    -ub 256 \
+    -c 81920 \
+    -t 8 \
+    --host 0.0.0.0 \
+    --port ${PORT} \
+    --api-key ${API_KEY} \
+    --nr-devices ${NUM_N3K} \
+    --slots \
+    --metrics \
+    --neutorch-cache-path ${CACHE_PATH} \
+    --chat-template ${CHAT_TEMPLATE} 2>&1 | tee /neuchips/llama.log
+
