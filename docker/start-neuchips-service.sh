c403b51e0041ff165db38d883cc984c4694c1be8
Rock Chen <rock.chen@neuchips.ai>
2025-07-15 16:21:55 +0800

    server: start server with parameters `--threads-http 1 --queued-http-reqs 3`         such that server will queue 3 http requests at most.
    - Update binaries from p334.
    
    Signed-off-by: Rock Chen <rock.chen@neuchips.ai>

--- docker/start-neuchips-service.sh
+++ docker/start-neuchips-service.sh
@@ -16,6 +16,8 @@ ${LLAMA_ROOT}/llama-server -m ${MODEL_PATH} \
     -ub 256 \
     -c 81920 \
     -t 8 \
+    --threads-http 1 \
+    --queued-http-reqs 3 \
     --host 0.0.0.0 \
     --port ${PORT} \
     --api-key ${API_KEY} \
